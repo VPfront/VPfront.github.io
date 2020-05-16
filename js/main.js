44121c39ef0ae629f8169596d23bee3d0c5c8ab2
Vlad <vladimirp911@gmail.com>
2020-05-16 18:54:10 +0200

    feat: add scrolTop

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) {
+/* document.addEventListener("DOMContentLoaded", function(event) {
     const modal = document.querySelector('.modal');
     const modalBtn =  document.querySelectorAll('[data-toggle=modal]');
     const closeBtn = document.querySelector('.modal__close');
@@ -9,6 +9,38 @@ document.addEventListener("DOMContentLoaded", function(event) {
         еlement.addEventListener('click', switchModal);
     });
     closeBtn.addEventListener('click', switchModal);
+}); */
+
+$(document).ready(function () {
+    var modal = $('.modal'),
+        modalBtn = $('[data-toggle=modal]'),
+        closeBtn = $('.modal__close');
+    
+        console.log(modal);
+        console.log(modalBtn);
+        console.log(closeBtn);
+        
+    modalBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    closeBtn.on('click', function () {
+        modal.toggleClass('modal--visible');
+    });
+    
 });
+jQuery(document).ready(function() {
+    var btn = $('.up');
+    $(window).scroll(function() {
+    if ($(window).scrollTop() > 700) {
+        btn.addClass('show');
+        } else {
+        btn.removeClass('show');
+        }
     
-    
+    });
+
+    btn.on('click', function(e) {
+        e.preventDefault();
+        $('html, body').animate({scrollTop:0}, '300');
+    });
+});
