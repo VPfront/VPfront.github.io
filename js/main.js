117ad8097daa1bdfca72d10201969662657e5667
Vlad <vladimirp911@gmail.com>
2020-05-18 00:22:18 +0200

    feat: add project section and   slide

--- js/main.js
+++ js/main.js
@@ -15,10 +15,6 @@ $(document).ready(function () {
     var modal = $('.modal'),
         modalBtn = $('[data-toggle=modal]'),
         closeBtn = $('.modal__close');
-    
-        console.log(modal);
-        console.log(modalBtn);
-        console.log(closeBtn);
         
     modalBtn.on('click', function () {
         modal.toggleClass('modal--visible');
@@ -26,7 +22,27 @@ $(document).ready(function () {
     closeBtn.on('click', function () {
         modal.toggleClass('modal--visible');
     });
+      //initialize swiper when document ready
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
     
+    });
+    var next = $('.swiper-button-next');
+    var prev = $('.swiper-button-prev');
+    var bullets = $('.swiper-pagination');
+    
+    next.css('left', prev.width() + 10 + bullets.width() + 10);
+    bullets.css('left', prev.width() + 14);
+
+
 });
 jQuery(document).ready(function() {
     var btn = $('.up');
